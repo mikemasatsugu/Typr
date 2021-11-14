805d2ee25ff3ea5110382ba7dcf1fe73d667b6ea
mikemasatsugu <mike.masatsugu@gmail.com>
2021-11-14 13:35:18 -1000

    spelling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles.scss';
 
 const Index = () => {
-  return <div>Welcome To FUCK</div>;
+  return <div>Welcome To React!</div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById('root'));
