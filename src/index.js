4e02a0ff1c6159d37e7c9f1cc436e99cb99ff5bb
mikemasatsugu <mike.masatsugu@gmail.com>
2021-11-14 12:39:55 -1000

    configured webpack, react, and express

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+
+const Index = () => {
+  return <div>Welcome to React!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById('root'));
